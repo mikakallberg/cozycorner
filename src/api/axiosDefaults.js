83c56bc7b0ba7e6e0b45442713132a2067162ae2
mikakallberg <mikaela.kallberg@hotmail.com>
2022-10-24 15:21:05 +0000

    Add images to assets, Add axiosDefault.js to api-file, To components add Assets.js, Avatar.js, MoreDropDowns.js, NotFound.js

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+
+axios.defaults.baseURL = 'https://portfolio-project-5-drf-api.herokuapp.com/'
+// axios.defaults.baseURL = 'https://3000-mikakallberg-cozycorner-5xekjfruf8l.ws-eu72.gitpod.io'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
