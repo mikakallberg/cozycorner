1f5701f7dfd815a45e1f5f02b162357b1959521a
mikakallberg <mikaela.kallberg@hotmail.com>
2022-10-26 22:29:00 +0000

    Remove commented out code from axiosDefault.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://portfolio-project-5-drf-api.herokuapp.com/'
-// axios.defaults.baseURL = 'https://3000-mikakallberg-cozycorner-5xekjfruf8l.ws-eu72.gitpod.io'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
 
