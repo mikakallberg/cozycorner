4455325be9082b0cb6ba77fb99dcd5811a8b0003
mikakallberg <mikaela.kallberg@hotmail.com>
2022-10-26 19:53:56 +0000

    Add popularprofiles to PostPage.js

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -10,6 +10,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import Comment from "../comments/Comment";
 import CommentCreateForm from "../comments/CommentCreateForm";
+import PopularProfiles from "../profiles/PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { fetchMoreData } from "../../utils/utils";
 import Asset from "../../components/Asset";
@@ -41,7 +42,7 @@ function PostPage() {
     return (
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
-                <p>Popular profiles for mobile</p>
+                <PopularProfiles mobile />
                 <Post {...post.results[0]} setPosts={setPost} postPage />
                 <Container className={appStyles.Content}>
                     {currentUser ? (
@@ -78,7 +79,7 @@ function PostPage() {
                 </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-                Popular profiles for desktop
+                <PopularProfiles />
             </Col>
         </Row>
     );
