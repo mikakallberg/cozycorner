7582b0a54247cb75410efa09431c3744ffa15221
mikakallberg <mikaela.kallberg@hotmail.com>
2022-10-28 09:11:14 +0000

    Change indentation

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -54,22 +54,21 @@ function PostCreateForm() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-    
-    const formData = new FormData();
-    
-    formData.append("title", title);
-    formData.append("content", content);
-    formData.append("image", imageInput.current.files[0]);
-    
-    try {
-        const { data } = await axiosReq.post("/posts/", formData);
-        history.push(`/posts/${data.id}`);
-    } catch (err) {
-        console.log(err);
-        if (err.response?.status !== 401) {
-            setErrors(err.response?.data);
+        const formData = new FormData();
+        
+        formData.append("title", title);
+        formData.append("content", content);
+        formData.append("image", imageInput.current.files[0]);
+        
+        try {
+            const { data } = await axiosReq.post("/posts/", formData);
+            history.push(`/posts/${data.id}`);
+        } catch (err) {
+            console.log(err);
+            if (err.response?.status !== 401) {
+                setErrors(err.response?.data);
+            }
         }
-    }
     };
 
     const textFields = (
